3f57245e95ce77311191c4da7320d5f75994304f
Nitin Garg <50919784+nitingargprft@users.noreply.github.com>
2023-01-04 14:31:56 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace GitDemo
     {
         static void Main(string[] args)
         {
-            var abc = "";
+            var abc = ""
             //This is test code with linting
             Console.WriteLine("Pr Template Test2");
         }
